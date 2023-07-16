50e957d1a8fa28c5e644220db9cd393167da2ad9
Oshani Kumarathunga <oshanipbkonline@gmail.com>
2023-07-16 19:40:13 +0530

    Updating todo list with HashMap

--- src/MorningRoutine.java
+++ src/MorningRoutine.java
@@ -2,5 +2,19 @@ public class MorningRoutine {
 
     public static void main(String[] args) {
         Todolist todo = new Todolist("My Morning Routine");
+
+        todo.addTask("Wake Up");
+        todo.addTask("Shower");
+        todo.addTask("Have Breakfast");
+        todo.displayTask();
+//        todo.removeTask(2);
+//        todo.displayTask();
+        todo.addTask("Do programming");
+        todo.markAsDone("Wake Up");
+        todo.displayTask();
+        todo.rename("Shower", "Take bath");
+        todo.rename("Have Breakfast", "Go for a run");
+        todo.displayTask();
+
     }
 }
--- src/Todolist.java
+++ src/Todolist.java
@@ -1,23 +1,31 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Todolist {
     private String topic;
-    private ArrayList<String> tasks;
+    private HashMap<String , Boolean> tasks;
 
     public Todolist(String topic) {
         this.topic = topic;
-        this.tasks = new ArrayList<String>();
+        this.tasks = new HashMap<String , Boolean>();
     }
 
     public void addTask(String taskName){
-        this.tasks.add(taskName);
+        this.tasks.put(taskName , false);
     }
 
-    public void removeTask(int taskNumber){
-        try {
-            this.tasks.remove(taskNumber);
-        }catch (IndexOutOfBoundsException e){
-            System.out.println("Could not remove task, no task at this index!");
+    public void removeTask(String taskName){
+//        try {
+//            this.tasks.remove(taskNumber);
+//        }catch (IndexOutOfBoundsException e){
+//            System.out.println("Could not remove task, no task at this index!");
+//        }
+        if (this.tasks.containsKey(taskName)){
+            System.out.println("Removing " + taskName);
+            this.tasks.remove(taskName);
+        }else {
+            System.out.println("No such a task");
         }
     }
 
@@ -25,19 +33,39 @@ public class Todolist {
         if (tasks.size() == 0){
             System.out.println("Our Todo List for " + topic + "is currently empty");
         }else {
-            System.out.println("Here is Todo List for " + topic);
-            for (int i=0; tasks.size() >= i; i++){
-                System.out.println("Task " + i + " is " + this.tasks.get(i));
+            System.out.println("Here is our Todo List for " + topic);
+            for(Map.Entry<String, Boolean> task : this.tasks.entrySet()) {
+                if (task.getValue()) {
+                    System.out.println("Task " + task.getKey() + " is complete");
+                }else {
+                    System.out.println("Task " + task.getKey() + " is not done");
+                }
             }
         }
     }
 
-    public void rename(int i, String newTask) {
-        try {
-            this.tasks.set(i,newTask);
+    public void rename(String oldTask, String newTask) {
+//        try {
+//            this.tasks.set(i,newTask);
+//        }
+//        catch (IndexOutOfBoundsException e) {
+//            System.out.println("Could not rename task, no task at this index!");
+//        }
+        System.out.println("Renaming " + oldTask + " to " + newTask);
+        if (this.tasks.containsKey(oldTask)){
+            this.tasks.put(newTask , this.tasks.get(oldTask));
+            this.tasks.remove(oldTask);
+        }else {
+            System.out.println("No such a task!");
         }
-        catch (IndexOutOfBoundsException e) {
-            System.out.println("Could not rename task, no task at this index!");
+    }
+
+    public void markAsDone(String taskName){
+        System.out.println("Marking " +taskName+ " as completed");
+        if (this.tasks.containsKey(taskName)){
+            this.tasks.put(taskName , true);
+        }else {
+            System.out.println("No such a task!");
         }
     }
 
